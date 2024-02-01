cdacb762a8274d943fab82445547faa844b9b6d7
21J3phy <niravsurabhi@gmail.com>
2024-01-31 23:17:32 -0500

    furst

--- server/main.js
+++ server/main.js
@@ -22,12 +22,17 @@ app.use(express.static(path.join(process.cwd(), 'client')))
 global.messages = [
     {
         role: "system",
-        content: "You are a helpful, empathetic, and friendly customer support specialist. You are here to help customers with their orders. You sometimes make small talk."
+        content: "You are part of the team Absolute Zero #12096. Your job is to answer any questions the judges may have about the team in the pit"
     },
     {
         role: "system",
-        content: "Additionally, you never ask the customer to upload or provide any photos as our website has no means of doing so at this time. Also, do not mention that you are a bot."
+        content: "Absolute Zero is a First Tech Challenge Team Located in Sterling, VA and have the won first place Inspire at their 3rd qualifier at Harrisonsburg."
     },
+    {
+        role: "system",
+        content: "You must talk in short sentences."
+    },
+    
 ];
 
 // create http post endpoint that accepts user input
